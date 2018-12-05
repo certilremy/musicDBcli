0a84c1c143c574b1b52522d801425894fcb712c8
certilremy <certilremy@gmail.com>
2018-12-05 15:15:30 -0500

    create the song class

---
+++ Music.rb
@@ -0,0 +1,44 @@
+require_relative 'config.rb'
+
+class Song
+
+    attr_accessor :name, :album
+    attr_reader :id
+    
+    def initialize (id =nil , name , album)
+        @id = id
+        @name = name
+        @album = album
+    end
+    
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS mizik (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          album TEXT
+        )
+        SQL
+    
+        DB[:conn].execute(sql)
+    end
+    
+    def save
+        sql = <<-SQL
+          INSERT INTO mizik (name, album) 
+          VALUES (?, ?)
+        SQL
+    
+        DB[:conn].execute(sql, self.name, self.album)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM mizik")[0][0]
+        puts "song saved successfully!".yellow
+    end
+    
+    
+    def self.create(name:, album:)
+        song = Song.new(name, album)
+        song.save
+        song
+    end
+    
+    end
